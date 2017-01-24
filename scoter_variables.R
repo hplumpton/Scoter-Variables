9bb0789ae3189f8543c0ac35f896bb780a7607dd
Hannah Plumpton <plumptonhannah@gmail.com>
2017-01-24 14:32:39 -0500

    started negative binomial regression for bathy, slope, and dist. does not like substrate and sedmobility gives me
    Error in model.frame.default(formula = Count ~ substrate, data = sco2,  :
      invalid type (list) for variable 'substrate'
    
    trying to fix

--- scoter_variables.R
+++ scoter_variables.R
@@ -40,33 +40,96 @@ sco2$bathy2=scale(sco2$bathy)
 substrate=readShapePoly("Layers/substrate/conmapsg.shp")
 proj4string(substrate)<-CRS("+proj=longlat +datum=WGS84")
 plot(substrate)
-summary(substrate)
-
-scoters=data.frame(read.csv("ObsData2.csv",header=TRUE))
-scoters <-na.omit(scoters)
-summary(scoters)
-
-coordinates(scoters)<-c("longitude_dd","latitude_dd") 
-#define x&y coordinates
-proj4string(scoters)<-CRS("+proj=longlat +datum=WGS84") 
-#assigning a projection
+names(substrate)
+substrate$AREA<-NULL
+substrate$PERIMETER<-NULL
+substrate$SEDIMENT<-NULL
 plot(scoters,add=TRUE)
 
-sco2=spTransform(scoters,CRS(proj4string(substrate))) 
-#assign same projection as substrate
 
 sco2$substrate=over(sco2,substrate)
 #extract substrate measure at each spatial location 
 #combined with scoters data
 
-#gives error in colMeans(x, na.rm=TRUE) : 'x' must be numeric
-sco2$substrate2=scale(sco2$substrate)
+
+head(sco2)
+
+#ocean floor slope
+
+
+slope<-terrain(bathy, opt=c('slope'), unit='degrees')
+summary(slope)
+head(slope)
+
+plot(scoters,add=TRUE)
+
+
+scotslope=SpatialPoints(sco2)
+proj4string(scotslope)<-CRS("+proj=longlat +datum=WGS84")
+sco2$slope=extract(slope,scotslope)
+#extract sediment mobility measure at each spatial location 
+#combined with scoters data
+sco2$slope2=scale(sco2$slope)
 #standardize covariates for comparison of beta estimates later on
+
+head(sco2)
+summary(sco2)
+
+
+#distance to shore
+
+shoreline=readShapePoly("Layers/shoreline/GSHHS_shp/i/GSHHS_i_L1.shp")
+proj4string(shoreline)<-CRS("+proj=longlat +datum=WGS84")
+
+seshoreline <- crop(shoreline, extent(-82, -72, 30, 39))
+proj4string(seshoreline)<-CRS("+proj=longlat +datum=WGS84")
+summary(seshoreline)
+plot(seshoreline)
+
+plot(scoters,add=TRUE)
+
+dist<-distanceFromPoints(seshoreline,scoters)
+proj4string(dist)<-CRS("+proj=longlat +datum=WGS84")
+head(dist)
+
+sco2$dist=extract(dist,sco2)
+#extract sediment mobility measure at each spatial location 
+#combined with scoters data
+sco2$dist2=scale(sco2$dist)
+#standardize covariates for comparison of beta estimates later on
+
 
 head(sco2)
 
 
+#negative binomial
+library(MASS)
+names(sco2)
 
+m1<-glm.nb(Count~bathy2, data=sco2)
+m1$aic
+
+m2<-glm.nb(Count~bathy2 + dist2, data=sco2)
+m2$aic
+
+m3<-glm.nb(Count~bathy2 + slope2, data=sco2)
+m3$aic
+
+m4<-glm.nb(Count~bathy2 + dist2 + slope2, data=sco2)
+m4$aic
+
+m5<-glm.nb(Count~dist2, data=sco2)
+m5$aic
+
+m6<-glm.nb(Count~dist2 + slope2, data=sco2)
+m6$aic
+
+m7<-glm.nb(Count~slope2, data=sco2)
+m7$aic
+
+m8<-glm.nb(Count~substrate, data = sco2)
+
+summary(sco2)
 
 #sediment mobility
 
@@ -74,6 +137,7 @@ sedmobility=readShapePoly("Layers/sediment mobility/SAB_median/SAB_median.shp")
 proj4string(sedmobility)<-CRS("+proj=longlat +datum=WGS84")
 plot(sedmobility)
 summary(sedmobility)
+
 
 scoters=read.csv("ObsData2.csv",header=TRUE)
 scoters <-na.omit(scoters)
@@ -89,92 +153,17 @@ sedmob<-spTransform(sedmobility, CRS("+proj=longlat +datum=WGS84"))
 #assigned same projection as sedmobility
 sco2=spTransform(scoters,CRS(proj4string(sedmobility))) 
 #assign same projection as sedmobility
-head(sedmobility)
+head(sedmob)
 
 scotsedmob=SpatialPoints(sco2)
 proj4string(scotsedmob)<-CRS("+proj=longlat +datum=WGS84")
-sco2$sedmobility=extract(sedmob,scotsedmob)
+sco2$sedmob=extract(sedmob,sco2)
 #extract sediment mobility measure at each spatial location 
 #combined with scoters data
-sco2$sedmobility2=scale(sco2$sedmobility)
+sco2$sedmobility2=scale(sco2$sedmob)
 #standardize covariates for comparison of beta estimates later on
 
-
-
-
-
-#ocean floor slope
-
-bathy=raster("Layers/etopo1 bathymetry.tif")
-image(bathy)
-summary(bathy)
-
-slope<-terrain(bathy, opt=c('slope'), unit='degrees')
-summary(slope)
-head(slope)
-
-scoters=read.csv("ObsData2.csv",header=TRUE)
-scoters <-na.omit(scoters)
-summary(scoters)
-
-coordinates(scoters)<-c("longitude_dd","latitude_dd") 
-#define x&y coordinates
-proj4string(scoters)<-CRS("+proj=longlat +datum=WGS84") 
-#assigning a projection
-plot(scoters,add=TRUE)
-
-sco2=spTransform(scoters,CRS(proj4string(bathy))) 
-#assign same projection as bathy
-
-scotslope=SpatialPoints(sco2)
-proj4string(scotslope)<-CRS("+proj=longlat +datum=WGS84")
-sco2$slope=extract(slope,scotslope)
-#extract sediment mobility measure at each spatial location 
-#combined with scoters data
-sco2$slope2=scale(sco2$slope)
-#standardize covariates for comparison of beta estimates later on
-
-
-
-
-
-#distance to shore
-
-shoreline=readShapePoly("Layers/shoreline/GSHHS_shp/i/GSHHS_i_L1.shp")
-proj4string(shoreline)<-CRS("+proj=longlat +datum=WGS84")
-
-seshoreline <- crop(shoreline, extent(-82, -72, 30, 39))
-proj4string(seshoreline)<-CRS("+proj=longlat +datum=WGS84")
-summary(seshoreline)
-plot(seshoreline)
-
-scoters=read.csv("ObsData2.csv",header=TRUE)
-scoters <-na.omit(scoters)
-summary(scoters)
-
-coordinates(scoters)<-c("longitude_dd","latitude_dd") 
-#define x&y coordinates
-proj4string(scoters)<-CRS("+proj=longlat +datum=WGS84") 
-#assigning a projection
-plot(scoters,add=TRUE)
-
-dist<-distanceFromPoints(seshoreline,scoters)
-proj4string(dist)<-CRS("+proj=longlat +datum=WGS84")
-head(dist)
-
-sco2=spTransform(scoters,CRS(proj4string(dist))) 
-#assign same projection as dist
-
-sco2$dist=extract(dist,sco2)
-#extract sediment mobility measure at each spatial location 
-#combined with scoters data
-sco2$dist2=scale(sco2$dist)
-#standardize covariates for comparison of beta estimates later on
-
-
-
-
-
+head(sco2)
 #transect data
 
 transect=readShapeLines("Layers/transects/WinterSurvey_TrackLines_sCoast.shp")
