30ad2a6bd1d3bcfe1314b855984d3473a1c0bfe8
plumptonhannah <plumptonhannah@gmail.com>
2017-07-21 16:03:36 -0400

    figured out Lasso nothing of significant for both Lassos, with and without random effects negative binominal didn't converge

--- scoter_variables.R
+++ scoter_variables.R
@@ -2608,15 +2608,79 @@ cv.lasso=cv.glmnet(x,year$Count)
 plot(cv.lasso)
 coef(cv.lasso)
 
-library(glmmLasso)
-lm1 <- glmmLasso(Count~bathy2+dist2+slope2+as.factor(sednum)+NAO2+wind2+wave2+as.factor(eco)+as.factor(bival), rnd = list(SrvyBgY=~1),
-                 family=quasipoisson(link = "log"), lambda=10, data = year)
-#Error in logLik.glmmLasso(y = y, mu = mu, family = family, ranef.logLik = NULL,  : 
-#object 'loglik' not found
 
-lm2<- glmmLasso(Count~bathy2+dist2+slope2+as.factor(sednum)+NAO2+wind2+wave2+as.factor(eco)+as.factor(bival), rnd = list(SrvyBgY=~1),
-                 family=quasipoisson, lambda=10, data = year)
-#Error: object of type 'closure' is not subsettable
+
+
+library(glmmLasso)
+logLik.glmmLasso<-function(y,mu,ranef.logLik=NULL,family,penal=FALSE, K = NULL) 
+{
+  fam <- family$family
+  
+  if(fam=="poisson")
+    loglik <- sum(y*log(mu)-mu)
+  
+  if(fam=="binomial")
+    loglik <- sum(log(mu[y==1]))+sum(log((1-mu)[y==0]))
+  
+  if(fam=="gaussian")
+    loglik <- sum(y*mu-0.5*(mu^2))
+  
+  if(fam=="acat")
+  {
+    mu_cat <- matrix(mu, byrow = TRUE, ncol = K)
+    mu_cat <- cbind(mu_cat,1-rowSums(mu_cat))
+    yhelp <- matrix(y, byrow = TRUE, ncol = K)
+    yhelp <- cbind(yhelp,1-rowSums(yhelp))
+    loglik <- sum(yhelp*log(mu_cat))
+  }
+  
+  if(fam=="cumulative")
+  {
+    mu_cat <- matrix(mu, byrow = TRUE, ncol = K)  
+    mu_help <- mu_cat
+    
+    
+    for (i in 2:K){
+      mu_cat[,i] <- mu_help[,i]-mu_help[,i-1]
+    }
+    
+    mu_cat <- cbind(mu_cat,1-mu_help[,K])
+    
+    yhelp <- matrix(y, byrow = TRUE, ncol = K)
+    ynew <- apply(yhelp,1,match,x=1)
+    ynew[is.na(ynew)] <- K+1
+    ynew <- factor(ynew)
+    yhelp <- as.matrix(model.matrix(~0+ynew, contrasts = list(ynew = "contr.treatment")))
+    
+    loglik <- sum(yhelp*log(mu_cat))
+    loglik
+  }
+  
+  if(penal)
+    loglik <- loglik + ranef.logLik 
+  
+  return(loglik)
+}
+lm1 <- glmmLasso(Count~bathy2+dist2+slope2+as.factor(sednum)+NAO2+wind2+wave2+as.factor(eco)+as.factor(bival), rnd = list(SrvyBgY=~1),
+                 family=poisson(link = "log"), lambda=.005, data = year)
+
+summary(lm1)
+lm2<- glmmLasso(Count~bathy2+dist2+slope2+as.factor(sednum)+NAO2+wind2+wave2+as.factor(eco)+as.factor(bival), rnd =list(SrvyBgY=~1),
+                 family=poisson(), lambda=100, data = year)
+
+summary(lm2)
+
+glmmLassoControl(lm2)
+
+is.factor(year$bival)
+year$sednum=as.factor(year$sednum)
+year$bival=as.factor(year$bival)
+m0a<-glmer.nb(Count~bathy2+dist2+slope2+sednum+NAO2+wind2+wave2+eco+bival+(1|SrvyBgY),data=year)
+
+
+
+
+
 
 
 #multicollinearity
